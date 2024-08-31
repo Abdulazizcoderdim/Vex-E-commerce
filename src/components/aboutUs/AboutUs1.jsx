e0f09c49cff5db93e0d2ca21a697413e95bcc0ce
AbdulazizRus <coderdim@gmail.com>
2024-08-31 09:19:54 +0500

    changes

--- src/components/aboutUs/AboutUs1.jsx
+++ src/components/aboutUs/AboutUs1.jsx
@@ -1,19 +1,17 @@
 import { Link } from 'react-router-dom';
 import './AboutUs.css';
 
-
 const AboutUs1 = () => {
-    return (
-        <div className='shop'>
-            <div className='shop__container'>
-                <h1>About Us</h1>
-                <div className='shop__link'>
-                   <Link to="/">Home</Link>
-                   / <Link to="/aboutUs">about us</Link>
-                </div>
-            </div>
+  return (
+    <div className="shop">
+      <div className="shop__container">
+        <h1>About Us</h1>
+        <div className="shop__link">
+          <Link to="/">Home</Link>/ <Link to="/aboutUs">about us</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs1;
